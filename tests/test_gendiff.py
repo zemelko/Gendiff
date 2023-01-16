c2b9e46aa659b9c4008c3cb716bd168ef2ef992a
Replit user <>
2023-01-16 12:12:04 +0000

    feat: Create tests, fixtures, expected Flatten.txt

---
+++ tests/test_gendiff.py
@@ -0,0 +1,22 @@
+from gendiff.scripts.gendiff import generate_diff
+import pytest
+
+
+@pytest.fixture
+def file1_json_flat():
+  return 'tests/fixtures/file1_flat.json'
+
+
+@pytest.fixture
+def file2_json_flat():
+  return 'tests/fixtures/file2_flat.json'
+
+
+@pytest.fixture
+def flatten_expected():
+  with open('tests/fixtures/expected/Flatten.txt') as f:
+    return f.read()
+
+    
+def test_json_flatten(file1_json_flat, file2_json_flat, flatten_expected):
+  assert flatten_expected == generate_diff(file1_json_flat, file2_json_flat)
