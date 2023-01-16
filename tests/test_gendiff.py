54b2d2a598ef8624d9743b62bd6487b27a7a13c3
Replit user <>
2023-01-16 15:03:30 +0000

    feat: Add YAML tests

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -4,19 +4,33 @@ import pytest
 
 @pytest.fixture
 def file1_json_flat():
-  return 'tests/fixtures/file1_flat.json'
+    return 'tests/fixtures/file1_flat.json'
 
 
 @pytest.fixture
 def file2_json_flat():
-  return 'tests/fixtures/file2_flat.json'
+    return 'tests/fixtures/file2_flat.json'
+
+
+@pytest.fixture
+def file1_yaml_flat():
+    return 'tests/fixtures/file1_flat.yaml'
+
+
+@pytest.fixture
+def file2_yaml_flat():
+    return 'tests/fixtures/file2_flat.yaml'
 
 
 @pytest.fixture
 def flatten_expected():
-  with open('tests/fixtures/expected/Flatten.txt') as f:
-    return f.read()
+    with open('tests/fixtures/expected/Flatten.txt') as f:
+        return f.read()
 
-    
+
 def test_json_flatten(file1_json_flat, file2_json_flat, flatten_expected):
-  assert flatten_expected == generate_diff(file1_json_flat, file2_json_flat)
+    assert flatten_expected == generate_diff(file1_json_flat, file2_json_flat)
+
+
+def test_yaml_flatten(file1_yaml_flat, file2_yaml_flat, flatten_expected):
+    assert flatten_expected == generate_diff(file1_yaml_flat, file2_yaml_flat)
