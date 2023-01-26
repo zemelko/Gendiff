55c8a5b7c7bebaa8afb49c379019e1cf28ff60ed
NikolayZemelko <zemelko1989@gmail.com>
2023-01-26 12:49:44 +0300

    feat: Add opportunity to choose format style

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -2,13 +2,20 @@ import argparse
 from gendiff.parser import pair_files_loader
 from gendiff.diff import diff_mapping
 from gendiff.formatters.stylish import stylish
+from gendiff.formatters.plain import plain
 
 
-def generate_diff(file1, file2, format=stylish):
+FORMATS = {
+    'stylish': stylish,
+    'plain': plain
+}
+
+
+def generate_diff(file1, file2, format='stylish'):
 
     dict1, dict2 = pair_files_loader(file1, file2)
     mapping = diff_mapping(dict1, dict2)
-    result = stylish(mapping)
+    result = FORMATS[format](mapping)
     return result
 
 
@@ -16,14 +23,13 @@ def main():
 
     desc = 'Compares two configuration files and whows a difference.'
     parser = argparse.ArgumentParser(description=desc)
-
-    parser.add_argument('-f', '--format', help='set format of output')
+    h = 'set format of output'
+    parser.add_argument('-f', '--format', help=h, default='stylish')
     parser.add_argument('file_1', metavar='first_file')
     parser.add_argument('file_2', metavar='second_file')
 
     args = parser.parse_args()
-    result = generate_diff(args.file_1, args.file_2)
-    print(result)
+    print(generate_diff(args.file_1, args.file_2, args.format))
 
 
 if __name__ == '__main__':
