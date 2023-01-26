f156f1ef06939dbc312746567e38ee957188f99c
NikolayZemelko <zemelko1989@gmail.com>
2023-01-26 16:55:02 +0300

    feat: Add diff_dict to json tests

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -59,6 +59,13 @@ def tree_plain_expected():
     with open('tests/fixtures/expected/plain/Tree.txt') as f:
         return f.read()
 
+
+@pytest.fixture
+def to_json_expected():
+    with open('tests/fixtures/expected/json/to_json.json') as f:
+        return f.read()
+
+
 # Stylish format
 def test_json_flatten(file1_json_flat, file2_json_flat, flatten_stylish_expected):
     assert flatten_stylish_expected == generate_diff(file1_json_flat, file2_json_flat)
@@ -75,6 +82,12 @@ def test_json_tree(file1_json_tree, file2_json_tree, tree_stylish_expected):
 def test_yaml_tree(file1_yaml_tree, file2_yaml_tree, tree_stylish_expected):
     assert tree_stylish_expected == generate_diff(file1_yaml_tree, file2_yaml_tree)
 
+
 # Plain format
 def test_json_tree(file1_yaml_tree, file2_yaml_tree, tree_plain_expected):
     assert tree_plain_expected == generate_diff(file1_yaml_tree, file2_yaml_tree, format='plain')
+
+
+# Json format
+def test_dict_to_json(file1_json_tree, file2_json_tree, to_json_expected):
+    assert to_json_expected == generate_diff(file1_json_tree, file2_json_tree, format='json')
